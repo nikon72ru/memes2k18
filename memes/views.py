77c470a000266b047ba8d3b12b1d5c61b78c75d6
Dominioncher <kushnirartyom@yandex.ru>
2018-11-10 19:55:01 +0500

    54

--- memes/views.py
+++ memes/views.py
@@ -1,15 +1,14 @@
 from django.shortcuts import render
 from django.template import loader
+from memes import models
 
 # Create your views here.
 from django.http import HttpResponse
 
 
 def fresh(request):
-    template = loader.get_template('memes/posts.html')
-    context = {
-    }
-    return HttpResponse(template.render(context, request))
+    memes = models.Meme.objects.all()
+    return render(request, 'memes/post_list.html', {'memes': memes})
 
 def upload(request):
     template = loader.get_template('memes/upload.html')
