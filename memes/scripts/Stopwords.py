d38b7c0531fa6cec8f4c84b414ed2c12c9d48420
nikon72ru <nikon72ru@gmail.com>
2018-11-10 23:17:12 +0300

    static

--- memes/scripts/Stopwords.py
+++ memes/scripts/Stopwords.py
@@ -1,7 +1,10 @@
 def Remove_Stopwords(phrase: list, stopwords_path):
-    text_file = open(stopwords_path, "r")
-    stop_words = text_file.read().lower().split('\n')
-    text_file.close()
+    # text_file = open(stopwords_path, "r")
+    # stop_words = text_file.read().lower().split('\n')
+    # text_file.close()
+    stop_words = [
+        'а', 'б'
+    ]
     filtered_word_list = phrase[:]
     for word in phrase:
         if word in stop_words: 
