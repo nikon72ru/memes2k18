dd9ac53c686c350bce8b1d00560d0149169dd80d
nikon72ru <nikon72ru@gmail.com>
2018-11-10 15:20:56 +0300

    static

--- memes/views.py
+++ memes/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
+from django.template import loader
 
 # Create your views here.
 from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    template = loader.get_template('memes/posts.html')
+    context = {
+    }
+    return HttpResponse(template.render(context, request))
--- memes2k18/settings.py
+++ memes2k18/settings.py
@@ -27,6 +27,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 # Application definition
 
@@ -122,4 +125,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/memes2k18/static/'
