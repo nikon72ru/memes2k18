03ffb7b1a5f2c3566b5153c210cd58be7297d6cf
unknown <bogdan.anastasia13@gmail.com>
2018-11-11 04:51:27 +0500

    new models

--- memes/scripts/Start.py
+++ memes/scripts/Start.py
@@ -11,28 +11,20 @@ def Start():
     memes = models.Meme.objects.all()
     for mem in memes:
         list_texts.append(' '.join(mem.lem_text.split(' ')).split())   
-        list_tags.append(' '.join(mem.labels.split(' ')).split())
-        
-    bigram = gensim.models.Phrases(list_texts, min_count=5, threshold=20)
-    bigram_mod = gensim.models.phrases.Phraser(bigram)
-    def make_bigrams(texts):
-        return [bigram_mod[doc] for doc in texts]
-
-    bigrams_texts = make_bigrams(list_texts)
-    dictionary_texts = gensim.corpora.Dictionary(bigrams_texts)
+        list_tags.append(' '.join(mem.labels.split(' ')).split())    
+    
+    dictionary_texts = gensim.corpora.Dictionary(list_texts)
     dictionary_texts.save('dictionary_texts.dict')
     
     dictionary_tags = gensim.corpora.Dictionary(list_tags)
     dictionary_tags.save('dictionary_tags.dict')
     
-    corpus_texts = [dictionary_texts.doc2bow(text) for text in bigrams_texts]
+    corpus_texts = [dictionary_texts.doc2bow(text) for text in list_texts]
     corpus_tags = [dictionary_tags.doc2bow(text) for text in list_tags]
     
-    
-    model_texts = DynamicTopics.compute_coherence_values(dictionary_texts, corpus_texts, bigrams_texts, 1, 10)
+    model_texts = DynamicTopics.compute_coherence_values(dictionary_texts, corpus_texts, list_texts, 1, 10)
     model_tags = DynamicTopics.compute_coherence_values(dictionary_tags, corpus_tags, list_tags, 1, 10)
-
-    .00000
+    
     for num in range(model_texts.num_topics):
         models.Cluster.objects.create(name = num, requests = 0, type = "text")
     for num in range(model_tags.num_topics):
@@ -43,30 +35,35 @@ def Start():
     i = 0
     for mem in memes:
         if(list_texts[i] != []):
-            res = num_cluster(list_texts, i, model_texts, corpus_texts)            
-            mem.cluster_text = models.Cluster.objects.get(name=res, type = 'text')
+            res = num_cluster(list_texts[i], i, model_texts, list_texts) 
+            mem.cluster_text = models.Cluster.objects.filter(name=res, type = 'text').last()
+        else:
+            mem.cluster_text = None
         if(list_tags[i] != []):
-            res = num_cluster(list_tags, i, model_tags, corpus_tags)
-            mem.cluster_label = models.Cluster.objects.get(name=res, type = 'tag')
+            res = num_cluster(list_tags[i], i, model_tags, list_tags)
+            mem.cluster_label = models.Cluster.objects.filter(name=res, type = 'tag').last()
+        else:
+            mem.cluster_text = None        
+        
         mem.save()
         i+=1
-        
       
     model_texts.save('model_texts.model')
     model_tags.save('model_tags.model')
 
     
-def num_cluster(data, i, model, corpus):
-    topic_keywords=[]
-    num_t = model.get_document_topics(corpus[i])
-    num_t.sort(key = lambda x: x[1], reverse = True)
-    wp = model.show_topic(num_t[0][0])
-    topic_keywords.append([word for word, prop in wp])
-    count = 0
-    for j in range(len(data[i])):
-        if data[i][j] in topic_keywords[0]:
-            count += 1
-    if count != 0:            
-        return num_t[0][0]
+def num_cluster(data, i, model, list_data):
+    topic_i = []
+    key_words_weight = 0
+    for num in range(model.num_topics):
+        for j in range(len(data)):
+            for word, prop in model.show_topic(num):
+                if data[j] == word:
+                    key_words_weight += prop
+        topic_i.append(key_words_weight)
+        key_words_weight = 0
+    
+    if max(topic_i) != 0:
+        return topic_i.index(max(topic_i))
     else:
         return -1
--- memes/scripts/TextSearch.py
+++ memes/scripts/TextSearch.py
@@ -3,21 +3,36 @@ import gensim
 from memes import models
 
 def getTopicForQuery(text, model, dictionary):
-    topic_keywords = []
-    ques_vec = dictionary.doc2bow(text)
-    topic_vec = model[ques_vec]
-    topic_vec = topic_vec[0]
-    topic_vec.sort(key = lambda x: x[1], reverse = True)
-    wp = model.show_topic(topic_vec[0][0])
-    topic_keywords.append([word for word, prop in wp])
-    count = 0
-    for j in range(len(text)):
-        if text[j] in topic_keywords[0]:
-            count += 1
-    if count != 0:        
-        return topic_vec[0][0]
+    topic_i = []
+    key_words_weight = 0
+    for num in range(model.num_topics):
+        for j in range(len(text)):
+            for word, prop in model.show_topic(num):
+                if text[j] == word:
+                    key_words_weight += prop
+        topic_i.append(key_words_weight)
+        key_words_weight = 0
+    
+    if max(topic_i) != 0:
+        return topic_i.index(max(topic_i))
     else:
         return -1
+    
+#    topic_keywords = []
+#    ques_vec = dictionary.doc2bow(text)
+#    topic_vec = model[ques_vec]
+#    topic_vec = topic_vec[0]
+#    topic_vec.sort(key = lambda x: x[1], reverse = True)
+#    wp = model.show_topic(topic_vec[0][0])
+#    topic_keywords.append([word for word, prop in wp])
+#    count = 0
+#    for j in range(len(text)):
+#        if text[j] in topic_keywords[0]:
+#            count += 1
+#    if count != 0:        
+#        return topic_vec[0][0]
+#    else:
+#        return -1
 
 def textSearch(text):
     text = Preprocessings.Preprocessings_phrase(text)
