c608b68ff08dda691de4c902fdc521322b119e8d
unknown <bogdan.anastasia13@gmail.com>
2018-11-11 02:01:12 +0500

    f

--- memes/scripts/DynamicTopics.py
+++ memes/scripts/DynamicTopics.py
@@ -4,18 +4,18 @@ def compute_coherence_values(dictionary, corpus, texts, start, step):
     num_topics = start;
     flag = True
     
-    model = New_model(corpus, num_topics, dictionary, 150, 200, 50, 0.15, 0.75, True)    
+    model = New_model(corpus, num_topics, dictionary, 150, 100, 50, 0.15, 0.75, True)    
     coherence_values.append(New_coherencemodel(model = model, texts = texts, dictionary = dictionary, coherence = 'u_mass'))
     
     num_topics += step;
-    model = New_model(corpus, num_topics, dictionary, 150, 200, 50, 0.15, 0.75, True)
+    model = New_model(corpus, num_topics, dictionary, 150, 100, 50, 0.15, 0.75, True)
     coherence_values.append(New_coherencemodel(model = model, texts = texts, dictionary = dictionary, coherence = 'u_mass'))
     if(coherence_values[-1] - coherence_values[-2] <= 0.01):
         flag = False
         return model
     num_topics += step;
     while(flag):         
-        model = New_model(corpus, num_topics, dictionary, 150, 200, 50, 0.15, 0.75, True)
+        model = New_model(corpus, num_topics, dictionary, 150, 100, 50, 0.15, 0.75, True)
         coh = New_coherencemodel(model = model, texts = texts, dictionary = dictionary, coherence = 'u_mass')
         if(coherence_values[-1] - coh <= 0.01):
             return model
