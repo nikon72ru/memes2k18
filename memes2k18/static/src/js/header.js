17e345a1eb3a59db437ec1d44a7fc4103c025af2
Dominioncher <kushnirartyom@yandex.ru>
2018-11-11 06:15:43 +0500

    vbhjk

--- memes2k18/static/src/js/header.js
+++ memes2k18/static/src/js/header.js
@@ -4,7 +4,7 @@ var header = document.getElementById("myHeader");
 var sticky = header.offsetTop;
 
 function myFunction() {
-    if (window.pageYOffset > sticky) {
+    if (window.pageYOffset > sticky && window.innerWidth>780) {
         header.classList.add("sticky");
     } else {
         header.classList.remove("sticky");
